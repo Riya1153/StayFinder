bb7169daa874344cd1d00f07bddb6950271faee9
Redi <riyamone7033@gmail.com>
2026-03-23 00:34:48 -0600

    StayFinder-03: Fixed commit sequence and added media URLs

--- stayfinder_backend/urls.py
+++ stayfinder_backend/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import hostel_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hostels/', hostel_list, name='hostel_list'),
 ]
+
+# Media files setup
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

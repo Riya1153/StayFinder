8c2e65db61bcf4e2fdfa037e477650b503625173
Redi <riyamone7033@gmail.com>
2026-03-23 11:50:14 -0600

    StayFinder-07: Separated front page and hostel list templates correctly

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Hostel
 
+# Ei function-ta missing chilo tai error ashche
+def front_page(request):
+    return render(request, 'front_page.html')
+
 def hostel_list(request):
     hostels = Hostel.objects.all()
     return render(request, 'hostel_list.html', {'hostels': hostels})
--- stayfinder_backend/urls.py
+++ stayfinder_backend/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import hostel_list
-from django.conf import settings
-from django.conf.urls.static import static
+from core.views import hostel_list, front_page # front_page import koro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', front_page, name='front_page'), # Eita empty path thakte hobe
     path('hostels/', hostel_list, name='hostel_list'),
 ]
-
-# Media files setup
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

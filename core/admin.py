2af893022f972ccb4548efdb33546ebedd38c9c9
Riya1153 <riyamone7033@gmail.com>
2026-04-25 21:44:48 +0600

    Added House model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Owner, Hostel
+from .models import Owner, Hostel, House
 
 admin.site.register(Owner)
 admin.site.register(Hostel)
+admin.site.register(House)
--- core/models.py
+++ core/models.py
@@ -13,13 +13,13 @@ class Owner(models.Model):
 class Hostel(models.Model):
     GENDER_CHOICES = [('G', 'Girls'), ('B', 'Boys')]
 
-    title = models.CharField(max_length=200)  # Ex: Matrichaya Hostel
-    location = models.CharField(max_length=255)  # Ex: Uttara, Dhaka
-    price_per_month = models.DecimalField(max_digits=10, decimal_places=2)  # 3500 BDT
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=255)
+    price_per_month = models.DecimalField(max_digits=10, decimal_places=2)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     stars = models.IntegerField(default=5)
 
-    # Facilities (Figma Page 6)
+    # Facilities
     has_wifi = models.BooleanField(default=True)
     has_cctv = models.BooleanField(default=True)
     has_food = models.BooleanField(default=False)
@@ -27,6 +27,18 @@ class Hostel(models.Model):
 
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='hostels/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class House(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=255)
+    rent = models.DecimalField(max_digits=10, decimal_places=2)
+    rooms = models.IntegerField(default=1)
+
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='houses/', null=True, blank=True)
 
     def __str__(self):
         return self.title
